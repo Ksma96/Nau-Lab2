437ef6b0630bc6b5aa5f47d3554b3b5b145754e4
Maksym Melanich <ksma727@gmail.com>
2023-06-04 22:04:36 +0300

    Lab2

---
+++ app/src/main/java/com/example/lab_2/DetailsFragment.kt
@@ -0,0 +1,40 @@
+package com.example.lab_2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.lab_2.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args: DetailsFragmentArgs by navArgs()
+        val food = args.food
+        binding.nameTextView.text = food.name
+        binding.descriptionTextView.text = food.description
+        Glide.with(requireContext()).load(food.url).into(binding.imageView)
+
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+}
